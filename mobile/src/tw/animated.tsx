a0db24a2b1b1b2a4f128282d5013d8b722433ba4
lollopanta <lollopanta09@gmail.com>
2026-03-26 11:48:34 +0100

    Fixed Tailwind animation

--- mobile/src/tw/animated.tsx
+++ mobile/src/tw/animated.tsx
@@ -1,7 +1,13 @@
-import * as TW from "./index";
+// Remove circular dependency on index.tsx
 import RNAnimated from "react-native-reanimated";
+import { View as RNView } from "react-native";
+import { useCssElement } from "react-native-css";
+
+const CSSView = (props: any) => {
+  return useCssElement(RNView, props, { className: "style" });
+};
 
 export const Animated = {
   ...RNAnimated,
-  View: RNAnimated.createAnimatedComponent(TW.View),
+  View: RNAnimated.createAnimatedComponent(CSSView),
 };
